2897e22c9352842cf235c9d09968c7ca97bc6772
Ornela Golloshi <ornelagolloshi25@gmail.com>
2025-11-02 16:03:47 -0500

    database-exploration

--- Database Exploration/02.sql
+++ Database Exploration/02.sql
@@ -1,2 +1,22 @@
 SELECT *
-FROM LaborStatisticsDB.dbo.anual_2016
+FROM LaborStatisticsDB.dbo.annual_2016
+
+
+SELECT *
+FROM LaborStatisticsDB.dbo.january_2017
+
+SELECT *
+FROM LaborStatisticsDB.dbo.datatype
+
+SELECT *
+FROM LaborStatisticsDB.dbo.datatype
+WHERE data_type_text LIKE '%women%';
+
+SELECT data_type_code, data_type_text
+FROM LaborStatisticsDB.dbo.datatype
+WHERE data_type_text LIKE '%women%';
+
+/*
+10	WOMEN EMPLOYEES
+39	WOMEN EMPLOYEES-TO-ALL EMPLOYEES RATIO
+*/
--- Database Exploration/03.sql
+++ Database Exploration/03.sql
@@ -4,6 +4,36 @@
 
 */
 
+select *
+from LaborStatisticsDB.dbo.supersector
+
+select *
+from LaborStatisticsDB.dbo.industry
+
 SELECT data_type_code, data_type_text
 FROM LaborStatisticsDB.dbo.datatype
 WHERE data_type_text LIKE '%women%';
+
+SELECT s.series_id, s.data_type_code,s.industry_code, s.supersector_code
+FROM LaborStatisticsDB.dbo.series as s
+INNER JOIN LaborStatisticsDB.dbo.datatype as dt
+ON s.data_type_code = dt.data_type_code
+INNER JOIN LaborStatisticsDB.dbo.industry as i
+on s.industry_code = i.industry_code
+INNER JOIN LaborStatisticsDB.dbo.supersector as ss
+on s.supersector_code = ss.supersector_code
+WHERE dt.data_type_text LIKE '%women%'
+    AND i.industry_name LIKE '%commercial banking%'
+    AND ss.supersector_name LIKE '%financial activities%'  
+
+SELECT s.series_id
+FROM LaborStatisticsDB.dbo.series as s
+INNER JOIN LaborStatisticsDB.dbo.datatype as dt
+ON s.data_type_code = dt.data_type_code
+INNER JOIN LaborStatisticsDB.dbo.industry as i
+on s.industry_code = i.industry_code
+INNER JOIN LaborStatisticsDB.dbo.supersector as ss
+on s.supersector_code = ss.supersector_code
+WHERE dt.data_type_text LIKE '%women%'
+    AND i.industry_name LIKE '%commercial banking%'
+    AND ss.supersector_name LIKE '%financial activities%'  
